70a2cc881a9667689311458a7ca9cf35b506d227
Saimonitsa <95011357+Saimonitsa@users.noreply.github.com>
2021-12-06 20:52:00 +0500

    step 10

--- ооп2.6/ооп2.6.cpp
+++ ооп2.6/ооп2.6.cpp
@@ -6,7 +6,7 @@ class Point {
 protected:
 	int x, y;
 public:
-	
+
 	Point() {
 		printf("Point()\n");
 		x = 0;
@@ -17,7 +17,7 @@ public:
 		this->x = x;
 		this->y = y;
 	}
-	Point(const Point &p) {
+	Point(const Point& p) {
 		printf("Point(const Point &p)\n");
 		x = p.x;
 		y = p.y;
@@ -38,7 +38,7 @@ void Point::reset() {
 	y = 0;
 }
 
-class ColoredPoint: public Point {
+class ColoredPoint : public Point {
 protected:
 	int color;
 public:
@@ -47,7 +47,7 @@ public:
 		printf("ColoredPoint()\n");
 		color = 0;
 	}
-	ColoredPoint(int x, int y, int color): Point (x, y)
+	ColoredPoint(int x, int y, int color) : Point(x, y)
 	{
 		printf("ColoredPoint(int x, int y)\n");
 		this->color = color;
@@ -66,45 +66,16 @@ public:
 	void change_color(int new_color) {
 		color = new_color;
 	}
-	
-};
 
-class Section {
-protected:
-	Point *p1;
-	Point *p2;
-
-public:
-	Section() {
-		printf("Section()\n");
-		p1 = new Point;
-		p2 = new Point;
-	}
-	Section(int x1, int y1, int x2, int y2) {
-		printf("Section(int x, int y, int x2, int y2)\n");
-		p1 = new Point(x1, y1);
-		p2 = new Point(x2, y2);
-	}
-	Section(const Section &s) {
-		printf("Section(const Section &p)\n");
-		p1 = new  Point(*(s.p1));
-		p2 = new  Point(*(s.p2));
-	}
-	~Section() {
-		//printf("%d, %d\n", x, y);
-		delete p1;
-		delete p2;
-		printf("~Section()\n");
-	}
-	
 };
 
 int main() {
-	{
-		Section *s1 = new Section;
-		Section *s2 = new Section(*s1);
-		delete s1;
-		delete s2; 
-		return 0;
-	}
+	Point* p1 = new ColoredPoint(1, 2, 42);
+	ColoredPoint* p2 = new ColoredPoint(1, 2, 42);
+
+	delete p1;
+	delete p2;
+
+	return 0;
+
 }
