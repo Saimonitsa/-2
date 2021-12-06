14e2ede86b0bf1cc9e5ac837af54c98440638fec
Saimonitsa <95011357+Saimonitsa@users.noreply.github.com>
2021-12-06 20:10:57 +0500

    step 2

---
+++ ооп2.2/ооп2.2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <conio.h>
+
+
+class Point {
+
+public:
+	int x, y;
+	Point() {
+		printf("Point()\n");
+		x = 0;
+		y = 0;
+	}
+	Point(int x, int y) {
+		printf("Point(int x, int y)\n");
+		this->x = x;
+		this->y = y;
+	}
+	Point(const Point& p) {
+		printf("Point(const Point &p)\n");
+		x = p.x;
+		y = p.y;
+	}
+	~Point() {
+		printf("%d, %d\n", x, y);
+		printf("~Point()\n");
+	}
+};
+
+int main() {
+	{
+		Point p;
+		Point p2(10, 20);
+		Point p3(p2);
+		return 0;
+	}
+}
